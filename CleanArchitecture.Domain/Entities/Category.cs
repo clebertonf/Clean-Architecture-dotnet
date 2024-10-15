d05e8744c01969db85d900e1e42a937d736c819e
Cleberton <clebertonfgc@gmail.com>
2024-10-14 22:23:33 -0300

    Add entities, interfaces and validation.

--- CleanArchitecture.Domain/Entities/Category.cs
+++ CleanArchitecture.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using CleanArchitecture.Domain.Validation;
+using System.Collections.Generic;
 
 namespace CleanArchitecture.Domain.Entities
 {
@@ -6,5 +7,29 @@ namespace CleanArchitecture.Domain.Entities
     {
         public string Name { get; set; }
         public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id <= 0, "Invalid id!");
+            ValidateDomain(name);
+            Id = id;
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name is required!");
+            DomainExceptionValidation.When(name.Length <= 3, "Name cannot be less than 3 characters!");
+            Name = name;
+        }
     }
 }
--- CleanArchitecture.Domain/Entities/Product.cs
+++ CleanArchitecture.Domain/Entities/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace CleanArchitecture.Domain.Entities
+﻿using CleanArchitecture.Domain.Validation;
+
+namespace CleanArchitecture.Domain.Entities
 {
     public class Product : BaseEntity
     {
@@ -9,5 +11,46 @@
         public string Image { get; private set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id Value!");
+            Id = Id;
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Name is required!");
+            DomainExceptionValidation.When(name.Length < 3,
+                "Name cannot be less than 3 characters!");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description),
+                "Description is required!");
+            DomainExceptionValidation.When(description.Length < 10,
+                "Name cannot be less than 10 characters!");
+            DomainExceptionValidation.When(price < 0,
+                "Invalid price value.");
+            DomainExceptionValidation.When(stock < 0,
+                "Invalid stock value.");
+            DomainExceptionValidation.When(image?.Length > 250,
+                "Image too long, maximum is 250 charactres!");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
     }
 }
